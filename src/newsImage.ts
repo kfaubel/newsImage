8eefb11f6356af6f903152e14140580a961c2db0
Ken Faubel <ken@faubel.org>
2021-02-21 11:10:06 -0500

    Fix PNG, tweaks to layout and logging

--- src/newsImage.ts
+++ src/newsImage.ts
@@ -4,11 +4,7 @@ import { doesNotReject } from "assert";
 // tslint:disable: object-literal-sort-keys
 const fs = require('fs');
 const axios = require('axios'); 
-const {Readable} = require('stream');
 const jpeg = require('jpeg-js');
-
-// const { createCanvas, loadImage } = require('canvas');
-const GoogleTopTenData = require('././newsdata');
 const pure = require('pureimage');
 // const pureTextPath = require('pureimage/src/text.js');
 // const pureregisterFont = require('pureimage/src/text.js');
@@ -22,6 +18,9 @@ module.exports = class newsimage {
     }
 
     public async getImage(dataItem) {
+        const title: string = `${dataItem.title}`
+        this.logger.info(`getImage: Title: ${title}`);
+
         const imageHeight: number = 1080; // 800;
         const imageWidth: number = 1920; // 1280;
 
@@ -34,10 +33,10 @@ module.exports = class newsimage {
         const DetailOffsetX: number = 60;
         const DetailOffsetY: number = 260;
 
-        const PictureX: number = 500;
-        const PictureY: number = 500;
+        const PictureX: number = 350;
+        const PictureY: number = 350;
         const PictureWidth: number = 400;
-        const PictureHeight: number = 500;
+        const PictureHeight: number = 650;
 
         const img = pure.make(imageWidth, imageHeight);
         const ctx = img.getContext('2d');
@@ -54,21 +53,33 @@ module.exports = class newsimage {
         ctx.fillRect(0,0,imageWidth, imageHeight);
 
         try {
+            this.logger.verbose(`PictureUrl: ${dataItem.pictureUrl}`);
             const response:any = await axios.get(dataItem.pictureUrl, {responseType: "stream"} );
-            const picture:any = await pure.decodeJPEGFromStream(response.data);
-            //await pure.encodeJPEGToStream(picture,fs.createWriteStream("picture.jpg"), 50);
-            const scaledWidth = (PictureHeight * picture.width) / picture.height;
-            ctx.drawImage(picture,
-                0, 0, picture.width, picture.height,             // source dimensions
-                PictureX, PictureY, scaledWidth, PictureHeight  // destination dimensions
-            );
+            let picture: any;
+
+            if (dataItem.pictureUrl.toUpperCase().endsWith("JPG")) {
+                picture = await pure.decodeJPEGFromStream(response.data);
+            } else if (dataItem.pictureUrl.toUpperCase().endsWith("PNG")) {
+                picture = await pure.decodePNGFromStream(response.data);
+            } else {
+                this.logger.warn(`Picture returned with unknown type (not jpg or png)`);
+                picture = null;
+            }
+
+            if (picture !== null) {
+                const scaledWidth = (PictureHeight * picture.width) / picture.height;
+                ctx.drawImage(picture,
+                    0, 0, picture.width, picture.height,             // source dimensions
+                    PictureX, PictureY, scaledWidth, PictureHeight  // destination dimensions
+                );
+            }
         } catch (e) {
-            this.logger.error("Failed to read picture: " + e);
+            this.logger.warn("Failed to read picture: " + e);
+            this.logger.warn("Stack: " + e.stack);
         }
 
         // Draw the title
-        const title: string = `${dataItem.title}`
-        this.logger.info(`Title: ${title}`);
+        
         const titleLines: string[] = this.splitLine(title, 48, 2);       
 
         let lineNumber: number = 0;
@@ -78,30 +89,27 @@ module.exports = class newsimage {
             ctx.fillText(titleLines[titleLine], TitleOffsetX, TitleOffsetY + (lineNumber++ * 80));
         }
 
-        lineNumber = 0;
-        const descriptionLines: string[] = this.splitLine(dataItem.description, 75, 3);
+        // lineNumber = 0;
+        // const descriptionLines: string[] = this.splitLine(dataItem.description, 75, 3);
 
-        for (const descriptionLine of Object.keys(descriptionLines)) {
-            ctx.fillStyle = textColor; 
-            ctx.font = "48pt 'alata-regular'";
-            ctx.fillText(descriptionLines[descriptionLine], DetailOffsetX, DetailOffsetY + (lineNumber++ * 80));            
-        }
-    
-        // await pure.encodeJPEGToStream(img, fs.createWriteStream(fileName), 50);
+        // for (const descriptionLine of Object.keys(descriptionLines)) {
+        //     ctx.fillStyle = textColor; 
+        //     ctx.font = "48pt 'alata-regular'";
+        //     ctx.fillText(descriptionLines[descriptionLine], DetailOffsetX, DetailOffsetY + (lineNumber++ * 80));            
+        // }
 
-        const jpegImg = jpeg.encode(img, 50);
-        // const jpegImgStream = new Readable({
-        //     read() {
-        //       this.push(jpegImg.data);
-        //       this.push(null);
-        //     }
-        // });
+        this.logger.verbose(`Encoding jpeg: ${title}`);
+
+        // Save the bitmap out to a jpeg image buffer
+        const jpegImg = await jpeg.encode(img, 50);
         
         // How long is this image good for
         const goodForMins = 60;
 
         const expires = new Date();
         expires.setMinutes(expires.getMinutes() + goodForMins);
+
+        this.logger.verbose(`getImage: Returning jpeg: ${title}`);
 
         return {
             expires: expires.toUTCString(),
